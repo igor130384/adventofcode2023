880f897c91f35a4c2685aec5d02f95edb5a0968e
Igor <GIV_84@mail.ru>
2023-12-08 13:40:24 +0400

    second commit

---
+++ My1.py
@@ -0,0 +1,23 @@
+stroki = []
+with open('first/input.txt', 'r') as file:
+    for i in file:
+        stroki.append(i)
+
+
+def digit(file):
+    d = []
+    for i in file:
+        a = []
+        for j in i:
+            if j.isdigit():
+                a.append(j)
+        if len(a) > 1:
+            d.append(int(''.join(a[:len(a):len(a) - 1])))
+        else:
+            for i in a:
+                d.append(int(i * 2))
+
+    return sum(d)
+
+
+print(digit(stroki))
